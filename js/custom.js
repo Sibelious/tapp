0a5c2541e112721c2a390dbafbbdb6e9d7c7efe5
Sibelious <Sibelious@outlook.com>
2014-02-25 03:17:03 +0000

    Added Functionality
    -Ability to send homework to the server.

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
             
   $('.controlPanel').hide();
   $('.menu').hide(); 
-  $('.UserSignUpForm').hide();      
+  $('.UserSignUpForm').hide();     
+  $('.hlForm').hide(); 
 
   $(".submit").click(function(event){
               event.preventDefault();
@@ -73,9 +74,63 @@ $(document).ready(function(){
               }
             });
     };
+
   $('#newUser').click(function(){
     $('.UserSignUpForm').fadeIn( 1000, function(){
       //done
     });
   });
+  $('#hltrigger').click(function(){
+    $('.hlForm').fadeIn(1000, function(){
+      //done
+    });
+  });
+
+
+
+  //Function to save a homework object
+       
+       $('#hlSubmit').click(
+        function saveit (){
+       var Homework = Parse.Object.extend("Homework");
+       var homework = new Homework();
+
+       var subject = $('#subjectSelector').val();
+       var studentName = $('#StudentName').val();
+       var hlcontent = $('#hltext').val();
+ 
+        homework.set("Subject", subject);
+        homework.set("UserName", studentName);
+        homework.set("Content", hlcontent);
+ 
+        homework.save(null, {
+          success: function(tableobject) {
+            // Execute any logic that should take place after the object is saved.
+            alert('New object created with objectId: ' + tableobject.id);
+            var SubjectSaveId = tableobject.id 
+          },
+          error: function(tableobject, error) {
+            // Execute any logic that should take place if the save fails.
+            // error is a Parse.Error with an error code and description.
+            alert('Failed to create new object, with error code: ' + error.description);
+          }
+        });
+    });
+
+       
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
